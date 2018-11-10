9ec7a55026b004d69b241bf74b44d48dc6c452b2
Ajmal Noushad <ajufhn@gmail.com>
2018-11-10 13:41:12 +0530

    Updates

---
+++ precache-manifest.0d716d5a1d60e2bfe18709269ac684b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bf55c6f8a6d5ccc79c8e5db1370a925",
+    "url": "/plain-portfolio/static/media/user.5bf55c6f.jpeg"
+  },
+  {
+    "revision": "0447f7c0a709efbe2b3e",
+    "url": "/plain-portfolio/static/js/runtime~main.0447f7c0.js"
+  },
+  {
+    "revision": "75d41464b2cb313c6ad4",
+    "url": "/plain-portfolio/static/js/main.75d41464.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/plain-portfolio/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "75d41464b2cb313c6ad4",
+    "url": "/plain-portfolio/static/css/main.c6aabe73.chunk.css"
+  },
+  {
+    "revision": "b150b160e88426331831961aef899780",
+    "url": "/plain-portfolio/index.html"
+  }
+];
